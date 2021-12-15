2d51ef7ce511c36d8edd4056e6495d68bcf1d6fb
Annnnya <anna.polova@ucu.edu.ua>
2021-12-15 12:30:26 +0200

    function that processes attack info

--- morskii_bii.py
+++ morskii_bii.py
@@ -7,10 +7,12 @@ def attack_checker(ship_list:list, attaked_pole:list, attacked_cell:tuple):
                 attaked_pole[attacked_cell[0]][attacked_cell[1]]='*'
                 repeat = True
                 kill = True
+                #тут я чекнула, чи попало в корабель
         if repeat:
             for section in ship:
                 if attaked_pole[section[0]][section[1]] != '*':
                     kill = False
+                #якщо попало, то чи вбило
         if kill:
             last_index=len(ship)-1
             for column in range(ship[0][0]-1, ship[last_index][0]+2):
@@ -18,15 +20,19 @@ def attack_checker(ship_list:list, attaked_pole:list, attacked_cell:tuple):
                     attaked_pole[column][row]='o'
             for section in ship:
                 attaked_pole[section[0]][section[1]] = '*'
+            #якщо вбило, заповняєм красиво
         if repeat:
             break
+        #якшо попало, нема сенсу далі проходитись
     if not repeat:
         try:
             attaked_pole[attacked_cell[0]][attacked_cell[1]]='o'
         except IndexError:
             repeat = True
+        #якщо атікована клітинка мимо поля, нехай робить ще раз
     for ship in ship_list:
         for section in ship:
             if attaked_pole[section[0]][section[1]] != '*':
                 win = False
+            #перевіряєм, чи ще лишились живі секції кораблів
     return attaked_pole, repeat, win
